93992890f73eb846cf86c3d7d2cd9873b5120578
Will Baymler <33941837+Will-B2@users.noreply.github.com>
2024-04-01 14:20:04 +0300

    add two tasks to project - please watch todo's

--- src/main/java/com/example/cryptoExchange/Controllers/HomeController.java
+++ src/main/java/com/example/cryptoExchange/Controllers/HomeController.java
@@ -1,18 +1,28 @@
 package com.example.cryptoExchange.Controllers;
 
+import com.example.cryptoExchange.model.CryptoCurrency;
+import com.example.cryptoExchange.service.CryptoCurrencyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    public final CryptoCurrencyService cryptoCurrencyService;
     @GetMapping("/home")
     public ModelAndView home() {
         return new ModelAndView("home");
     }
     @GetMapping("/cryptoCurrencyList")
-    public ModelAndView cryptoCurrencyList() {
-        return new ModelAndView("cryptoCurrencyList");
+    public List<CryptoCurrency> cryptoCurrencyList() {
+//        return new ModelAndView("cryptoCurrencyList");
+        // TODO: сделать вывод динамической информации из базы данных
+        return cryptoCurrencyService.getAllCryptoCurrencies();
     }
     @GetMapping("/auth")
     public ModelAndView auth() {
