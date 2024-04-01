1a57867e83d7d45b6caf3eccae9e5273cf480002
Will Baymler <33941837+Will-B2@users.noreply.github.com>
2024-04-01 13:54:33 +0300

    fix class structure of project

--- src/main/java/com/example/cryptoExchange/repository/PocketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cryptoExchange.repository;
-
-import com.example.cryptoExchange.model.Pocket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PocketRepository extends JpaRepository<Pocket, Long> {
-}
